360390cd5c6aa574b9913c9390516e1c0ab5a6d4
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-11-30 17:19:48 -0300

    requisitos 3,4,5 e 6 concluidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css';
+import { useState } from 'react';
 import Form from './components/Form';
 import Title from './components/Title';
 
 function App() {
+  const [formVisible, setFormVisible] = useState(false);
   return (
     <div>
       <Title />
-      <Form />
+      {formVisible && <Form cancelButton={ () => setFormVisible(false) } />}
+      {!formVisible && (
+        <button onClick={ () => setFormVisible(true) }>
+          Cadastrar nova senha
+        </button>
+      )}
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,34 @@
-function Form() {
+import { useState } from 'react';
+
+type FormProps = {
+  cancelButton: () => void;
+};
+
+function Form({ cancelButton }: FormProps) {
+  const [serviceName, setServiceName] = useState('');
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [url, setUrl] = useState('');
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
+
+  const isPasswordMin = password.length >= 8;
+  const isPasswordMax = password.length <= 16;
+  const passwordNumbersAndLetters = /[a-zA-Z]/.test(password) && /\d/.test(password);
+  const passwordSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  const validPasswordClass = 'valid-password-check';
+  const invalidPasswordClass = 'invalid-password-check';
+
+  const validForm = () => {
+    const nameValid = !!serviceName.trim();
+    const loginValid = !!login.trim();
+    const passwordValid = isPasswordMin
+        && isPasswordMax
+        && passwordNumbersAndLetters
+        && passwordSpecialChars;
+
+    setIsButtonVisible(nameValid && loginValid && passwordValid);
+  };
   return (
     <form>
       <div>
@@ -6,6 +36,8 @@ function Form() {
         <input
           type="text"
           id="serviceName"
+          value={ serviceName }
+          onChange={ (event) => { setServiceName(event.target.value); validForm(); } }
         />
       </div>
       <div>
@@ -13,6 +45,8 @@ function Form() {
         <input
           type="text"
           id="login"
+          value={ login }
+          onChange={ (event) => { setLogin(event.target.value); validForm(); } }
         />
       </div>
       <div>
@@ -20,22 +54,48 @@ function Form() {
         <input
           type="password"
           id="password"
+          value={ password }
+          onChange={ (event) => { setPassword(event.target.value); validForm(); } }
         />
       </div>
+      <ul>
+        <li
+          className={ !isPasswordMin
+            ? invalidPasswordClass : validPasswordClass }
+        >
+          Possuir 8 ou mais caracteres
+        </li>
+        <li
+          className={ !isPasswordMax
+            ? invalidPasswordClass : validPasswordClass }
+        >
+          Possuir até 16 caracteres
+        </li>
+        <li
+          className={ !passwordNumbersAndLetters
+            ? invalidPasswordClass : validPasswordClass }
+        >
+          Possuir letras e números
+        </li>
+        <li
+          className={ !passwordSpecialChars
+            ? invalidPasswordClass : validPasswordClass }
+        >
+          Possuir algum caractere especial
+        </li>
+      </ul>
+
       <div>
         <label htmlFor="url">URL </label>
         <input
           type="text"
           id="url"
+          value={ url }
+          onChange={ (event) => setUrl(event.target.value) }
         />
       </div>
-      <div>
-        <button>Cadastrar</button>
-      </div>
-      <div>
-        <button>Cancelar</button>
-      </div>
-
+      <button disabled={ !isButtonVisible }>Cadastrar</button>
+      <button onClick={ cancelButton }>Cancelar</button>
     </form>
   );
 }
